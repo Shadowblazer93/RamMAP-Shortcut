06966c7d84ce25c1b5cfa65a820a7d4cb4aee16a
Shadowblazer93 <66685463+Shadowblazer93@users.noreply.github.com>
2021-04-02 08:40:59 +0530

    Beta v1
    Fully functioning beta

--- ClearRam.bat
+++ ClearRam.bat
@@ -1,5 +1,3 @@
-set filepath=C:/Users/farah/src/RamMAP.exe
-
-START /MAX %filepath% -Ew
-START /MAX %filepath% -Em
-START /MAX %filepath% -Es
+START /MAX RamMAP.exe -Ew
+START /MAX RamMAP.exe -Em
+START /MAX RamMAP.exe -Es
